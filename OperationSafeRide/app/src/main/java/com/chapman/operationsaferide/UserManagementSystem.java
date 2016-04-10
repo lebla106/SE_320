0877002fb7ef319a126d68264858f3b290cc3cb8
benav111 <benav111@mail.chapman.edu>
2016-04-10 12:29:23 -0700

    Couldn't commit in android studio so i edited the file

--- OperationSafeRide/app/src/main/java/com/chapman/operationsaferide/UserManagementSystem.java
+++ OperationSafeRide/app/src/main/java/com/chapman/operationsaferide/UserManagementSystem.java
@@ -1,7 +1,44 @@
 package com.chapman.operationsaferide;
-
+import java.sql.*
 /**
  * Created by klamkin on 4/4/16.
  */
 public class UserManagementSystem {
+//doesnt follow class diagram exactly because it would not work how we want it to so
+    //necessary changes made
+    public User addUser(User aUser)
+    {
+        //adds aUser to the database
+        //wil later return user but currently there is no user class
+        //will not add user if it already exists
+        //everything commented out to avoid errors
+        //we also dont have access to this database
+    }
+
+    public User deleteUser(User aUser)
+    {
+        //deletes aUser from the database
+        //wil later return user but currently there is no user class
+        //everything commented out to avoid errors
+    }
+
+    public String getUserState(User aUser)
+    {
+        return user.userState;
+    }
+
+    public void setUserState(String state,User aUser)
+    {
+        aUser.userState = state;
+    }
+
+    public void setUserType(String type,User aUser)
+    {
+        aUser.userType = type;
+    }
+
+    public String getUserType(User aUser)
+    {
+        retun aUser.userType;
+    }
 }
