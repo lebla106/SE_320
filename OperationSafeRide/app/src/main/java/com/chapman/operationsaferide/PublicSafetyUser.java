452a2c69cbe99158f3cbfea2526378d17b55772c
benda106 <benda106@mail.chapman.edu>
2016-04-13 13:35:56 -0700

    Added javadoc

--- OperationSafeRide/app/src/main/java/com/chapman/operationsaferide/PublicSafetyUser.java
+++ OperationSafeRide/app/src/main/java/com/chapman/operationsaferide/PublicSafetyUser.java
@@ -1,67 +1,119 @@
 package com.chapman.operationsaferide;
 
 /**
+ * @author      Amit Ben-David <benda106@mail.chapman.edu>
+ * @version     1.0                 (current version number of program)
+ * @since       2016-04-9          (the version of the package this class was first added to)
+ *
  * Class representing any employee of Operation Safe Ride from Chapman University
  */
 public class PublicSafetyUser extends User {
 
+    /**
+     * The Public Safety User's name.
+     */
     private String name;
-    private int employeeID;
+
+    /**
+     * The Public Safety User's ID number.
+     */
+    private int empID;
+
+    /**
+     * TThe Public Safety User's state.
+     */
     private String state;
+
+    /**
+     * The Public Safety User's type.
+     */
     private String type;
 
     public PublicSafetyUser()
     {
-        super("state1", "PublicSafety", "Unknown");
+        super("state1", "PublicSafety");
     }
 
     public PublicSafetyUser(String n, int id, String s)
     {
-        super(s, "PublicSafety", n);
-        employeeID = id;
+        super(s, "PublicSafety");
+        name = n;
+        empID = id;
     }
 
-    //EmployeeID
+
+    /**
+     * Sets the Public Safety User's ID number
+     * @param  id number from which the Public Safety User's ID
+     */
     public void setID(int id)
     {
-        this.employeeID = id;
+        this.empID = id;
     }
 
+    /**
+     * Gets the Public Safety User's ID number
+     * @return  the Public Safety User's ID number
+     */
     public int getID()
     {
-        return this.employeeID;
+        return this.empID;
     }
 
-    //User Name
+    /**
+     * Sets the Public Safety User's Name
+     * @param  userName string to set Public Safety User's name
+     */
     public void setName(String userName)
     {
         this.name = userName;
     }
 
+    /**
+     * Gets the Public Safety User's name
+     * @return  the Public Safety User's name
+     */
     public String getName()
     {
         return this.name;
     }
 
-    // user state
+
+    /**
+     * Gets the Public Safety User's state
+     * @return  the Public Safety User's state
+     */
     public String getUserState()
     {
         return state;
     }
 
+    /**
+     * Sets the Public Safety User's state
+     * @param  s string to set the User's state
+     */
     public void setUserState(String s)
     {
         state = s;
     }
 
-    // user type
+    /**
+     * Gets the Public Safety User's type
+     * @return  the Public Safety User's type
+     */
     public String getUserType()
     {
         return type;
     }
 
+    /**
+     * Sets the Public Safety User's type
+     * @param  t string to set the Public Safety User's type
+     */
     public void setUserType(String t)
     {
         type = t;
     }
+
+
 }
