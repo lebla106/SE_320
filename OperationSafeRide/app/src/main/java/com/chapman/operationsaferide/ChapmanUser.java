a040df0acb0f586a435c9e9c5d75440824c66c33
Melissa L <lebla106@users.noreply.github.com>
2016-04-13 00:14:48 -0700

    Update ChapmanUser.java

--- OperationSafeRide/app/src/main/java/com/chapman/operationsaferide/ChapmanUser.java
+++ OperationSafeRide/app/src/main/java/com/chapman/operationsaferide/ChapmanUser.java
@@ -1,8 +1,18 @@
 package com.chapman.operationsaferide;
-
-/*
-  Class representing any user of Operation Safe Ride from Chapman University (Chapman Faculty, Staff or Students)
+ 
+ /**
+ * Created by Melissa LeBlanc on 4/5/16.
+ * @author Melissa LeBlanc
+ * 
+ * This class inherits from the User class, which contains one 
+ * string attribute called name. It contains the attributes: 
+ * chapmanID, numGuests, state, type,pickupLocation, and dropoffLocation. It represents the user 
+ * side of the OperationSafeRide app and has various associated
+ * functions including sending a ride request (as an email) to Public Safety.
+ * 
  */
+ 
+ 
 public class ChapmanUser extends User
 {
 	private String name;
@@ -15,6 +25,11 @@ public class ChapmanUser extends User
 	//private Map dropoffLocation;
 
 	//*********************************************** Constructor ***************************************************
+	
+	/**
+     * Public constructor to create an instance of this class
+     * 
+     */
 	public ChapmanUser()
 	{
         super("state1", "ChapmanStudent");
@@ -25,7 +40,16 @@ public class ChapmanUser extends User
 		//pickup Location = new LocationService();
 		//dropoffLocation = new Map();
 	}
-
+	
+	
+    /**
+     * Overloaded constructor 
+	 * @param n A String representing user name
+	 * @param id An int representing user chapmanID
+	 * @param numG An int representing number of guests
+	 * @param s A string representing state
+     *
+     */
     public ChapmanUser(String n, int id, int numG, String s)
     {
         super(s, "ChapmanStudent");
@@ -39,86 +63,147 @@ public class ChapmanUser extends User
 
 	//****************************************** Getters and Setters *************************************************
 
-	//ChapmanID
+	/**
+	* Sets chapmanID
+	* @param id
+	* @return void
+	*/
 	public void setID(int id)
 	{
 		this.chapmanID = id;
 	}
 
+	/**
+	* Gets chapmanID
+	* @return int
+	*/
 	public int getID()
 	{
 		return this.chapmanID;
 	}
 
-	//User Name
+	/**
+	* Sets user Name
+	* @param userName
+	* @return void
+	*/
 	public void setName(String userName)
 	{
 		this.name = userName;
 	}
 
+	/**
+	* Gets user Name
+	* @return String
+	*/
 	public String getName()
 	{
 		return this.name;
 	}
 
-	//Number of Guests
+	/**
+	* Sets number of guests
+	* @param guests
+	* @return void
+	*/
 	public void setNumGuests(int guests)
 	{
 		this.numGuests = guests;
 	}
 
+	/**
+	* Gets number of guests
+	* @return int
+	*/
 	public int getNumGuests()
 	{
 		return this.numGuests;
 	}
 
-    // user state
+   /**
+	* Gets user state
+	* @return String
+	*/
     public String getUserState()
     {
         return state;
     }
 
+	/**
+	* Sets user state
+	* @param s
+	* @return void
+	*/
     public void setUserState(String s)
     {
         state = s;
     }
 
-    // user type
+	/**
+	* Gets user type
+	* @return String
+	*/
     public String getUserType()
     {
         return type;
     }
-
+	
+	/**
+	* Sets user type
+	* @param t
+	* @return void
+	*/
     public void setUserType(String t)
     {
         type = t;
     }
 
-	/*
-	//Pickup Location
-	public void setPickupLocation(Map m)
-	{
-		this.pickupLocation = m;
-	}
-
-	public Map getPickupLocation()
-	{
-		return this.pickupLocation;
-	}
-
-	//Dropoff Location
-	public void setDropoffLocation(Map m)
-	{
-		this.dropoffLocation = m;
-	}
-
-	public Map getDropoffLocation()
-	{
-		return this.dropoffLocation;
-	}
+	/**
+	* Sets pickupLocation
+	* @param m
+	* @return void
 	*/
+	
+	//public void setPickupLocation(Map m)
+	//{
+	//	this.pickupLocation = m;
+	//}
+	
+	/**
+	* Gets pickupLocation
+	* @return Map
+	*/
+	//public Map getPickupLocation()
+	//{
+	//	return this.pickupLocation;
+	//}
+
+	/**
+	* Sets dropoffLocation
+	* @param m
+	* @return void
+	*/
+	//public void setDropoffLocation(Map m)
+	//{
+	//	this.dropoffLocation = m;
+	//}
+
+	/**
+	* Gets dropoffLocation
+	* @return Map
+	*/
+	//public Map getDropoffLocation()
+	//{
+	//	return this.dropoffLocation;
+	//}
+	
 
 	//*********************************************** Override toString ************************************************
+	
+	/**
+	* Override toString
+	* @return String
+	*/
 	public String toString()
 	{
 		//Overriding toString method
@@ -128,9 +213,17 @@ public class ChapmanUser extends User
 
 
 	//*********************************************** Other Functions *************************************************
-	/*
-	Function that sends email to PSafe notifying them of ride request with associated location of requester as well as
-	Chapman ID and name of user and the total number of guests
+	/**
+	* Function that sends email to PSafe notifying them of ride request with associated 
+	* location of requester as well as Chapman ID and name of user and the total 
+	* number of guests. Returns whether it was successful or not.
+	* @param pickupLocation
+	* @param dropoffLocation
+	* @param numGuests
+	* @param name
+	* @param id
+	* 
+	* @return boolean
 	*/
 
 	/*
